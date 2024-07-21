1fa669666bb2330982737e11ddcf0d5d0c5c6060
vikas4488 <40414161+vikas4488@users.noreply.github.com>
2024-07-21 14:52:56 +0530

    Update settings.py

--- testcurd/settings.py
+++ testcurd/settings.py
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 """
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'testcurd.urls'
@@ -160,16 +161,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-MEDIA_ROOT = '/home/vikas04488/plant/bubble/static/media'
-MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/vikas04488/plant/bubble/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+#MEDIA_ROOT = '/home/vikas04488/plant/bubble/static/media'
+#MEDIA_URL = '/media/'
+#STATIC_ROOT = '/home/vikas04488/plant/bubble/static'
+#STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
