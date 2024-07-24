4a9a7351c4615ff557d450f8680b526daf3d5402
vikas4488 <40414161+vikas4488@users.noreply.github.com>
2024-07-25 00:04:27 +0530

    Update settings.py

--- testcurd/settings.py
+++ testcurd/settings.py
@@ -162,10 +162,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')] 
+STATICFILES_DIRS = /plantstorage/static
+#STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_ROOT = /plantstorage/staticfiles
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = /plantstorage/media/
 
 #STATIC_URL = 'static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
