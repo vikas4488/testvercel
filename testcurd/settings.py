653b3b231ac8b43493aa3f262aa70e92f7b9f7d9
vikas4488 <40414161+vikas4488@users.noreply.github.com>
2024-07-25 23:26:16 +0530

    setting updated

--- testcurd/settings.py
+++ testcurd/settings.py
@@ -171,7 +171,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = '/railway/staticfiles'
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
-MEDIA_ROOT = os.path.join('/railway', 'media')
+#MEDIA_ROOT = os.path.join('/railway', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'railway/media/')
 #MEDIA_ROOT = '/railway/media/'
 
 #STATIC_URL = 'static/'
